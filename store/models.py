80a95c937b08d38540db37cedcf5d5399b78a9aa
nahiyananwar <nahiyanorko@gmail.com>
2025-10-08 00:45:24 +0600

    added migrations

--- store/models.py
+++ store/models.py
@@ -15,7 +15,7 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     product_update = models.DateTimeField(auto_now=True)
 
